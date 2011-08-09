58aa6ba8b59df00217692928b4e5c07d8d93bab0
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-08-09 18:06:50 +0530

    ...

--- main.hs
+++ main.hs
@@ -1,1 +1,33 @@
 -- hello
+module Main where
+import Data.List as L
+
+stringToInt::String->Int
+stringToInt str = read str
+
+
+getkdistpairs::[Int]->[Int]->Int->[(Int,Int)]
+getkdistpairs _ [] _ = []
+getkdistpairs (x:xs) (y:ys) k 
+    | (x+k)==y = (x,y):getkdistpairs xs ys k
+    | (x+k)<y  = getkdistpairs xs (y:ys) k
+    | (x+k)>y  = getkdistpairs (x:xs) ys k
+
+
+countkapart::[Int]->Int->Int
+countkapart (v:vs) k = let aux1 = dropWhile (\x -> k > (x-v)) vs
+                           aux = getkdistpairs (v:vs) aux1 k      
+                       in length aux          
+                             
+
+
+solve::Int->Int->[Int]->Int
+solve n k vals = countkapart (L.sort vals) k
+
+main = 
+    do nkstr<-getLine
+       valsstr<-getLine
+       let [n,k] = map stringToInt $ words nkstr
+           vals = map stringToInt $ words valsstr
+       print $ solve n k vals
+
